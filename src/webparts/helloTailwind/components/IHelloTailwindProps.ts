356a538151740aeeac5a7c2f9e6e104e8831dd98
Ari Gunawan <ari.gunawan.3023@gmail.com>
2021-03-11 11:37:21 +0700

    Initial generated code

---
+++ src/webparts/helloTailwind/components/IHelloTailwindProps.ts
@@ -0,0 +1,3 @@
+export interface IHelloTailwindProps {
+  description: string;
+}
---
+++ src/webparts/helloTailwind/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IHelloTailwindWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'HelloTailwindWebPartStrings' {
+  const strings: IHelloTailwindWebPartStrings;
+  export = strings;
+}
